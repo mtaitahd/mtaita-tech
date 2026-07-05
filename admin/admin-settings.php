ed037c78bb2da3bde391912e6a1507f7c42ba060
mtaitahd <mtaitahd@gmail.com>
2026-07-05 15:50:23 +0300

    Remove Settings.php require from admin-settings.php to isolate

--- admin/admin-settings.php
+++ admin/admin-settings.php
@@ -2,9 +2,8 @@
 $page_title = 'Settings';
 $active_page = 'settings';
 require_once 'admin_header.php';
-require_once __DIR__ . '/../lib/Settings.php';
 ?>
 <div class="admin-card">
-    <p>Settings page content will appear here.</p>
+    <p>Settings page works without Settings.php include.</p>
 </div>
 <?php require_once 'admin_footer.php'; ?>
